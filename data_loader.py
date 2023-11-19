b9aeff50b4933382a58145a6dfec9f9853ea9e45
willC707 <91046237+willC707@users.noreply.github.com>
2023-11-19 11:45:52 -0500

    Add files via upload

---
+++ data_loader.py
@@ -0,0 +1,53 @@
+
+from sklearn.datasets import fetch_lfw_people
+from sklearn.datasets import fetch_lfw_pairs
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report
+from model import FacialRecognitionModel
+from keras.utils import to_categorical
+from sklearn.preprocessing import normalize
+import numpy as np
+from skimage.transform import resize
+import matplotlib.pyplot as plt
+
+
+if __name__ == "__main__":
+
+    print('Loading Data')
+    # lfw_people = fetch_lfw_pairs(data_home="./data", subset='train', color=True, resize=None)
+    lfw_people = fetch_lfw_people(data_home="./data", color=True, resize=None, min_faces_per_person=70, funneled=False)
+    # lfw_pairs = fetch_lfw_pairs(data_home="./data",color=True, resize=None,funneled=False,slice_=None)
+    # lfw_people.images = normalize(lfw_people.images.reshape((lfw_people.images.shape[0], -1)))
+    print(lfw_people.images.shape)
+    print(lfw_people.target.shape)
+
+    print('Splitting Data')
+    X_train, X_test, y_train, y_test = train_test_split(lfw_people.images,lfw_people.target, test_size=0.25, random_state=42)
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.10, random_state=42)
+    X_test_lr = resize(X_test, (X_test.shape[0], 13, 9, 3))
+    plt.imshow(X_test_lr[0])
+    plt.show()
+    X_test_lr = resize(X_test_lr, (X_test_lr.shape[0], 125, 94, 3), anti_aliasing=True, order=0)
+    plt.imshow(X_test_lr[0])
+    plt.show()
+    plt.imshow(X_test[0])
+    plt.show()
+
+    print(X_train.shape)
+    print(y_train.shape)
+
+    # X_train = X_train.astype('float32') / 255
+    # X_test = X_test.astype('float32') / 255
+    # X_train = np.reshape(X_train, (X_train.shape[0], 250, 250, 3))
+    # X_test = np.reshape(X_test, (X_test.shape[0], 250, 250, 3))
+
+    model = FacialRecognitionModel()
+    model.train(X_train.reshape(-1, 125,94,3), to_categorical(y_train), X_val.reshape(-1, 125,94,3), to_categorical(y_val))
+
+    model.test(X_test.reshape(-1, 125,94,3), to_categorical(y_test))
+
+
+    model.test(X_test_lr.reshape(-1, 125, 94,3), to_categorical(y_test))
---
+++ model.py
@@ -0,0 +1,29 @@
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from keras.optimizers import Adam
+from keras.losses import categorical_crossentropy
+
+
+class FacialRecognitionModel:
+    def __init__(self, in_shape=(125,94,3)):
+        self._model = Sequential()
+
+        self._model.add(Conv2D(32, (5,5), activation='relu', input_shape=in_shape))
+        self._model.add(MaxPooling2D(pool_size=(2,2)))
+        self._model.add(Conv2D(64, (3,3,) , activation='relu'))
+        self._model.add(MaxPooling2D(pool_size=(2,2)))
+        self._model.add(Conv2D(128, kernel_size=(3,3), activation='relu'))
+        self._model.add(MaxPooling2D(pool_size=(2,2)))
+        self._model.add(Flatten())
+        self._model.add(Dense(units=256, activation='relu'))
+        self._model.add(Dense(units=7, activation='softmax'))
+
+        self._model.compile(optimizer=Adam(learning_rate=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
+
+    def train(self, X_train, y_train, X_val, y_val):
+        self._model.fit(X_train, y_train, epochs=20, batch_size=64, validation_data=(X_val, y_val))
+
+    def test(self, X_test, y_test):
+        score = self._model.evaluate(X_test, y_test, verbose=0)
+        print(f'Test loss: {score[0]:.4f}')
+        print(f'Test accuracy: {score[1]:.4f}')
